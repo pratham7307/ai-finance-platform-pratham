a22066632a6fb18938df011a9f1cdb3905cca07d
pratham7307 <raipratham8957@gmail.com>
2026-02-05 01:07:30 +0530

    Dashboard Page created and account drawer

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { Toaster } from 'sonner';
 const inter=Inter({subsets:["latin"]});
 
 export const metadata = {
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
         {/*header*/}
         <Header></Header>
         <main className="min-h-screen">{children}</main>
+        <Toaster richColors/>
         {/*footer*/}
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-600">
